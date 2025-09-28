8c0bec90ec8904eda9c4ed227acff985472d9611
Nicolas Salazar <nicolas.salazar.castillo@correounivalle.edu.co>
2025-09-28 13:58:37 -0500

    Fixes to breadth first search algorithm

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -5,12 +5,12 @@ from aux_functions import is_in_grid_bounds, expand_node
 # Function to find possible solutions
 def breadth_first_search(problem):
     node = Node(problem.initial, avaible_samples=list(problem.samples))
-    frontier = problem.frontier
+    frontier = Queue()
     reached = problem.reached
 
-    frontier.insert(0, node)
+    frontier.en_queue(node)
     while frontier:
-        node = frontier.pop()
+        node = frontier.de_queue()
 
         is_goal = problem.check_state(node)
         if is_goal:
@@ -19,4 +19,4 @@ def breadth_first_search(problem):
         for child in expand_node(node, problem):
             if child.get_state() not in reached:
                 reached[child.get_state()] = child
-                frontier.insert(0, child)
+                frontier.en_queue(child)
